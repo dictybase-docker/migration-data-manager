059d2fcb7f2d4c325d0e1bd11c020a99ce8034f1
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-10-27 22:34:28 -0500

    removed hook script

--- hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ${ETCD_CLIENT_SERVICE_HOST+defined} ]
-then
-    curl http://${ETCD_CLIENT_SERVICE_HOST}:${ETCD_CLIENT_SERVICE_PORT}/v2/keys/migration/download -XPUT -d value="complete"
-else
-    echo host not found
-fi
